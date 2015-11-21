b119008140c075a36c598bca9abf06c32bf9d36b
Cecilia <xic.cecilia@gmail.com>
2015-11-21 16:33:26 +0800

    Create common.h

---
+++ scr/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef long LONG;
+
+#endif
